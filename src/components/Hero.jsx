168d58c6d7f15c69192c53d203edf93a9ce27ca6
anoz05 <159677775+anojpradhan@users.noreply.github.com>
2025-07-28 15:01:17 +0545

    Hero nai ho

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,8 @@ const Hero = () => {
     <section className="min-h-screen flex flex-col md:flex-row items-center justify-between bg-blue-100 px-30 py-16">
       {/* Left: Text */}
       <div className="flex-1 text-center md:text-left">
-        <h1 className="text-4xl md:text-8xl font-bold mb-4">Hi, I'm Anoz</h1>
-        <p className="text-2xl">Frontend Developer</p>
+        <h1 className="text-4xl md:text-8xl font-bold mb-4">Hi, I'm Anoz Pradhan </h1>
+        <p className="text-2xl">Fullstack Developer</p>
       </div>
     </section>
   );
